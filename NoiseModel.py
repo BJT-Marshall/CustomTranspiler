Cleaned up documentation in all files and fixed bug where parameter based errors are not applied to controlled rotation gates.

NoiseModel.py
@@ -2,14 +2,16 @@ import numpy as np
 from qiskit import QuantumCircuit
 from qiskit_aer.noise import NoiseModel, depolarizing_error, thermal_relaxation_error
 from qiskit.circuit.library import RXGate, RYGate, RZGate, CRXGate, CRYGate, CRZGate
-from qiskit.converters import circuit_to_dag
+from qiskit.converters import circuit_to_dag, dag_to_circuit
 
 class CustomNoise(NoiseModel):
-
+    """
+    Custom noise model to simulate the errors of superconducting qubit-based hardware.
+    """
     
     def __init__(self, basis_gates=None):
         super().__init__(basis_gates)
-        #Setting new attributes for to be accessed during optimization
+        #Setting attributes for to be accessed during optimisation
         self.single_qubit_depol = 2.5e-4
         self.two_qubit_depol = 2.5e-3
 
@@ -32,7 +34,7 @@ class CustomNoise(NoiseModel):
         Model Constants:
         :param T1: Thermal t1 relaxation time constant, default = 150 \\mu s
         :type T1: float
-        :param T2: Thermal t2 relaxation time constant, default =75 \\mu s
+        :param T2: Thermal t2 relaxation time constant, default = 75 \\mu s
         :type T2: float
         :param time_single_qubit: Single qubit gate thermal relaxation time, default = 25ns
         :type time_single_qubit: float
@@ -62,17 +64,24 @@ class CustomNoise(NoiseModel):
         
         #Rotation gate errors:
         model.add_all_qubit_quantum_error(single_qubit_thermal, instructions = ["rx","ry","rz"])
-        rotation_depol_errors = rotation_based_noise(circuit, single_qubit_depol_param)
-        for gate_label in rotation_depol_errors:
-            model.add_all_qubit_quantum_error(error = rotation_depol_errors[gate_label], instructions = gate_label)
+        rotation_depol_errors_1q = rotation_based_noise(circuit, single_qubit_depol_param)
+        for gate_label in rotation_depol_errors_1q:
+            model.add_all_qubit_quantum_error(error = rotation_depol_errors_1q[gate_label], instructions = gate_label)
         
         #Two qubit noise channels
         two_qubit_thermal = thermal_relaxation_error(t1 = T1, t2 = T2, time = time_two_qubit)
         two_qubit_depol = depolarizing_error(param = two_qubit_depol_param, num_qubits=2) #depolorizing error rate ~0.0025
-
         two_qubit_error = two_qubit_depol.compose(two_qubit_thermal)
-        for gate in ["cx","cy","cz","crx","cry","crz"]:
+        for gate in ["cx","cy","cz"]:
             model.add_all_qubit_quantum_error(error=two_qubit_error, instructions=gate)
+
+        #Two qubit rotation gate errors:
+        for gate in ["crx","cry","crz"]:
+            model.add_all_qubit_quantum_error(error=two_qubit_thermal, instructions=gate)    
+        
+        rotation_depol_errors_2q = rotation_based_noise(circuit, two_qubit_depol_param)
+        for gate_label in rotation_depol_errors_2q:
+            model.add_all_qubit_quantum_error(error = rotation_depol_errors_2q[gate_label], instructions = gate_label)
 
         self.model = model
 
@@ -81,6 +90,15 @@ class CustomNoise(NoiseModel):
 #Helper methods for the CustomNoise class:
 
 def rotation_based_noise(circuit: QuantumCircuit, base_depol_error_rate: float):
+    """
+    Generates depolarizing errors for each rotation gate dependant on its rotation parameter, to be appended into the custom noise model.
+
+    :param circuit: The quantum circuit being considered, from which the rotation gates will be taken
+    :type circuit: QuantumCircuit
+    :param base_depol_error_rate: The default depolarization error rate for the rotation gate type
+    :returns rotation_errors: A dictionary containing the rotation gate error channels indexed by their parameter label
+    :rtype: dict
+    """
     
     dag = circuit_to_dag(circuit)
 
@@ -103,10 +121,21 @@ def rotation_based_noise(circuit: QuantumCircuit, base_depol_error_rate: float):
     return rotation_errors
             
 def parameter_adjustment(base_error:float, param:float):
+    """
+    Mathematical model to define parameter based depolarization error rates for rotation gates. Larger rotations produce higher errors
+    due to build up or errors through multiple smaller rotations in real hardware. 
+    Adjusted errors are in the range :math:`(0,10b]` where :math:`b` is the base depolarization error rate passed through :python:`base_error`.
 
-    #To be a mathematical model that punishes larger rotations to account for the build up of errors from multiple shorter rotation pulses  
-    error = 10*base_error*abs(np.sin(param/4)) #ranges from one order of magnitude larger than the base rate to zero 
-                                               #(~10^-3 on the high end using default values to zero for appplication sof identity.)
+    :param base_error: The default depolarization error rate for the rotation gate type
+    :type base_error: float
+    :param param: The rotation parameter to be adjusted
+    :type param: float
+    :returns error: The adjusted depolarization error rate
+    :rtype: float
+    """
+  
+    error = 10*base_error*abs(np.sin(param/4)) 
+
     return error
 
 